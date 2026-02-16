4adb80f3fcd27074e7325c79fbfb2f917cdc2ab8
volkat0r <volkerderksen@gmail.com>
2026-02-16 20:48:53 +0100

    first small layout changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+  },
+];
