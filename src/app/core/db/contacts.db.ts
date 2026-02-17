b2460deca2809f564958d55a0481f1272d62428c
LevinKrischer <levin@levinkrischer.de>
2026-02-17 08:43:56 +0100

    feature: definine functions for users/contact for add, set, update and delete

--- src/app/core/db/contacts.db.ts
+++ src/app/core/db/contacts.db.ts
@@ -1,23 +1,70 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { SupabaseClientService } from './supabase.client';
+
+export interface Contact {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  color: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class ContactsDb {
+
+  contacts = signal<Contact[]>([]);
+
   constructor(private supa: SupabaseClientService) { }
 
-  getContacts() {
+  async getContacts() {
+    const { data: contacts, error } = await this.supa.supabase
+      .from('contacts')
+      .select('*');
+
+    if (error) {
+      console.error('[Supabase] Error loading contacts:', error.message);
+      return;
+    }
+
+    if (!contacts) return;
+    this.contacts.set(contacts || []);
+  }
+
+  async setContact(contact: Omit<Contact, 'id'>) {
+    const { data, error } = await this.supa.supabase
+      .from('contacts')
+      .insert([{ ...contact }])
+      .select();
+
+    if (error) {
+      console.error('[Supabase] Error adding contact:', error.message);
+      return;
+    }
 
   }
 
-  addContact() {
+  async updateContact(id: number, update: Partial<Contact>) {
+    const { data, error } = await this.supa.supabase
+      .from('contacts')
+      .update(update)
+      .eq('id', id)
+      .select();
 
+    if (error) {
+      console.error('[Supabase] Error updating contact:', error.message);
+      return;
+    }
   }
 
-  updateContact() {
+  async deleteContact(id: number) {
+    const { error } = await this.supa.supabase
+      .from('contacts')
+      .delete()
+      .eq('id', id);
 
-  }
-
-  deleteContact() {
-
+    if (error) {
+      console.error('[Supabase] Error deleting contact:', error.message);
+      return;
+    }
   }
 }
--- src/app/core/db/users.db.ts
+++ src/app/core/db/users.db.ts
@@ -1,23 +1,69 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { SupabaseClientService } from './supabase.client';
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  color: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class UsersDb {
+
+  users = signal<User[]>([]);
+
   constructor(private supa: SupabaseClientService) { }
 
-  getUsers() {
+  async getUsers() {
+    const { data: users, error } = await this.supa.supabase
+      .from('users')
+      .select('*');
 
+    if (error) {
+      console.error('[Supabase] Error loading users:', error.message);
+      return;
+    }
+
+    if (!users) return;
+    this.users.set(users || []);
   }
 
-  setUser(user: { name: string; email: string , password: string , color: string }) {
+  async setUser(user: Omit<User, 'id'>) {
+    const { data, error } = await this.supa.supabase
+      .from('users')
+      .insert([{ ...user }])
+      .select();
 
+    if (error) {
+      console.error('[Supabase] Error adding user:', error.message);
+      return;
+    }
   }
 
-  updateUser(id: number, user: { name: string; email: string, password: string, color: string }) {
+  async updateUser(id: number, update: Partial<User>) {
+    const { data, error } = await this.supa.supabase
+      .from('users')
+      .update(update)
+      .eq('id', id)
+      .select();
 
+    if (error) {
+      console.error('[Supabase] Error updating user:', error.message);
+      return;
+    }
   }
 
-  deleteUser(id: number) {
+  async deleteUser(id: number) {
+    const { error } = await this.supa.supabase
+      .from('users')
+      .delete()
+      .eq('id', id);
 
+    if (error) {
+      console.error('[Supabase] Error deleting user:', error.message);
+      return;
+    }
   }
 }
