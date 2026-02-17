59316fa02e68e1a64392727dfb3ba231491a146b
volkat0r <volkerderksen@gmail.com>
2026-02-17 19:37:04 +0100

    styling: main-navi active

--- src/app/layout/navi/navi.ts
+++ src/app/layout/navi/navi.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navi',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterModule],
   templateUrl: './navi.html',
   styleUrl: './navi.scss',
 })
