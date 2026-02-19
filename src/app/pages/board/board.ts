1e4101f34a98213a312c16e849a8df0d617efeab
LevinKrischer <levin@levinkrischer.de>
2026-02-19 14:51:50 +0100

    refact: add input-components into edit-contact-form

--- src/app/pages/board/board.ts
+++ src/app/pages/board/board.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ContactAddFormComponent } from '../../components/contact-add-form/contact-add-form';
 import { CommonModule } from '@angular/common';
+import { ContactEditFormComponent } from "../../components/contact-edit-form/contact-edit-form";
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [ContactAddFormComponent, CommonModule],
+  imports: [ContactAddFormComponent, CommonModule, ContactEditFormComponent],
   templateUrl: './board.html',
   styleUrl: './board.scss',
 })
