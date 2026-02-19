2841b5e7b567ae8682e6424e423644fb702de462
mohabroha@mail.de <mohabroha@mail.de>
2026-02-19 08:12:00 +0100

    Refactor contacts section: update contact list, contact details and styling

--- src/app/services/supabase.ts
+++ src/app/services/supabase.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { createClient } from '@supabase/supabase-js';
 import { environment } from '../../environments/environment';
-
 @Injectable({
   providedIn: 'root',
 })
