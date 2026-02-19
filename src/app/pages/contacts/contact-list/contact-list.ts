37ecb0e6caa5aec1f116ce9e3293e22763c22231
Patrick <patrick_schauer@t-online.de>
2026-02-19 20:12:08 +0100

    feat: style ContactList with buttons

--- src/app/pages/contacts/contact-list/contact-list.ts
+++ src/app/pages/contacts/contact-list/contact-list.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { GroupedContacts, ContactWithInitials } from '../../../core/db/contacts.db';
 import { ContactAddFormComponent } from '../../../components/contact-add-form/contact-add-form';
+import { Button } from '../../../shared/ui/button/button';
 
 
 @Component({
   selector: 'app-contact-list',
-  imports: [ContactAddFormComponent, CommonModule],
+  imports: [ContactAddFormComponent, CommonModule, Button],
   templateUrl: './contact-list.html',
   styleUrl: './contact-list.scss',
 })
