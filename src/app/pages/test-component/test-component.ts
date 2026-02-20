837a264195ea40ea156bba2ea84f4687d440faec
Patrick <patrick_schauer@t-online.de>
2026-02-20 12:34:07 +0100

    feat: integrate transparent search input; add it to UI & TEST page

--- src/app/pages/test-component/test-component.ts
+++ src/app/pages/test-component/test-component.ts
@@ -2,10 +2,16 @@ import { CommonModule } from '@angular/common';
 import { ContactAddFormComponent } from './../../components/contact-add-form/contact-add-form';
 import { Component } from '@angular/core';
 import { Button } from '../../shared/ui/button/button';
+import { InputFieldComponent } from '../../shared/ui/input-field/input-field';
 
 @Component({
   selector: 'app-test-component',
-  imports: [ContactAddFormComponent, Button, CommonModule],
+  imports: [
+    ContactAddFormComponent,
+    Button,
+    InputFieldComponent,
+    CommonModule
+  ],
   templateUrl: './test-component.html',
   styleUrl: './test-component.scss',
 })
