554063b9b8833090d72ef5a1530affdfeacb3bb5
volkat0r <volkerderksen@gmail.com>
2026-02-20 09:34:50 +0100

    Test: adding Test site for ui elements

--- src/app/pages/board/board.ts
+++ src/app/pages/board/board.ts
@@ -1,25 +1,10 @@
 import { Component } from '@angular/core';
-import { ContactAddFormComponent } from '../../components/contact-add-form/contact-add-form';
-import { CommonModule } from '@angular/common';
-import { ContactEditFormComponent } from '../../components/contact-edit-form/contact-edit-form';
-import { Button } from '../../shared/ui/button/button';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [ContactAddFormComponent, CommonModule, Button],
+  imports: [],
   templateUrl: './board.html',
   styleUrl: './board.scss',
 })
-export class Board {
-  // Beispiel:
-  isContactModalOpen = false;
-
-  openModal() {
-    this.isContactModalOpen = true;
-  }
-
-  closeModal() {
-    this.isContactModalOpen = false;
-  }
-}
+export class Board {}
