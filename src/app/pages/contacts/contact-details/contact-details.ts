1ccf17db80eadfc106da089bd5273cacf8b99617
Patrick <patrick_schauer@t-online.de>
2026-02-23 15:04:47 +0100

    feat: final mobile tayloring of ContactDetails

--- src/app/pages/contacts/contact-details/contact-details.ts
+++ src/app/pages/contacts/contact-details/contact-details.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostListener, ElementRef } from '@angular/core';
 import { ContactWithInitials } from '../../../core/db/contacts.db';
 import { ContactEditFormComponent } from '../../../components/contact-edit-form/contact-edit-form';
 
@@ -16,8 +16,26 @@ export class ContactDetails {
   @Output() back = new EventEmitter<void>();
 
   isContactModalOpen = false;
+  isMobileActionsOpen = false;
 
-  // NEU: eigener State für den Kontakt, der editiert wird
+  constructor(private elRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event) {
+    if (this.isMobileActionsOpen && !this.elRef.nativeElement.querySelector('.mobile-actions-fab')?.contains(event.target)) {
+      this.isMobileActionsOpen = false;
+    }
+  }
+
+  toggleMobileActions() {
+    this.isMobileActionsOpen = !this.isMobileActionsOpen;
+  }
+
+  closeMobileActions() {
+    this.isMobileActionsOpen = false;
+  }
+
+
   selectedContact: ContactWithInitials | null = null;
 
   openModal() {
@@ -29,8 +47,8 @@ export class ContactDetails {
   }
 
   openEdit(contact: ContactWithInitials) {
-    this.selectedContact = contact;   // Kontakt setzen
-    this.isContactModalOpen = true;   // Modal öffnen
+    this.selectedContact = contact;
+    this.isContactModalOpen = true;
   }
 
   onEditSaved() {
