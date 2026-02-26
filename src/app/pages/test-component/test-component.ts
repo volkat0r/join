29d6b3959b5d994b78c5265a9eb2b0f0f6c1912c
Patrick <patrick_schauer@t-online.de>
2026-02-26 13:07:37 +0100

    feat: @UI & TEST: add contact-picker component

--- src/app/pages/test-component/test-component.ts
+++ src/app/pages/test-component/test-component.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
+import { inject, signal } from '@angular/core';
 import { ContactAddFormComponent } from './../../components/contact-add-form/contact-add-form';
 import { Component } from '@angular/core';
 import { Button } from '../../shared/ui/button/button';
 import { InputFieldComponent } from '../../shared/ui/forms/input-field/input-field';
+import { ContactPicker } from '../../shared/ui/contact-picker/contact-picker';
+import { ContactsDb } from '../../core/db/contacts.db';
 
 /**
  * A testing or demonstration component used to orchestrate UI elements.
@@ -12,11 +15,22 @@ import { InputFieldComponent } from '../../shared/ui/forms/input-field/input-fie
  */
 @Component({
   selector: 'app-test-component',
-  imports: [ContactAddFormComponent, Button, InputFieldComponent, CommonModule],
+  imports: [ContactAddFormComponent, Button, InputFieldComponent, CommonModule, ContactPicker],
   templateUrl: './test-component.html',
   styleUrl: './test-component.scss',
 })
 export class TestComponent {
+  contactsDb = inject(ContactsDb);
+  selectedContactIds = signal<number[]>([]);
+
+  async ngOnInit() {
+    await this.contactsDb.getContacts();
+  }
+
+  onContactsSelected(ids: number[]) {
+    this.selectedContactIds.set(ids);
+  }
+
   /** * Controls the visibility state of the contact creation modal.
    * @default false
    */
