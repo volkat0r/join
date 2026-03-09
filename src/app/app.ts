dc6be8d87b36b9383f84c6ef291d1400620cc85e
Patrick <patrick_schauer@t-online.de>
2026-03-09 13:18:01 +0100

    feat: test pseudo-help route guard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { SupabaseService } from './services/supabase';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,20 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.scss', '../../public/assets/styles/fonts.scss'],
 })
 export class App {
+  private supabaseService = inject(SupabaseService);
+  private router = inject(Router);
+
   protected readonly title = signal('join');
+
+
+  async ngOnInit() {
+    const { data } = await this.supabaseService.getSession();
+
+    if (data.session) {
+      this.router.navigate(['/board']);
+    } else {
+      this.router.navigate(['/help']); // /login
+    }
+  }
+
 }
